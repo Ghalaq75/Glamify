9a36f02a730b08a1b7a2a0332637ab3d03697af2
Ghalaq75 <ghalaq75@gmail.com>
2026-05-03 00:00:17 +0300

    add

--- api-server/src/app.js
+++ api-server/src/app.js
@@ -13,8 +13,13 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json({ limit: '15mb' }));
+app.use(cors({
+    origin: [
+      'http://localhost:5173',
+      'https://glamify-frontend.onrender.com'
+    ],
+    credentials: true
+  }));app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ limit: '15mb', extended: true }));
 
 app.use('/api', healthRoutes);
